08c2eea1b9bbf1231464d5a64a6bf92ef0f35c82
CascadePass <106619481+CascadePass@users.noreply.github.com>
2025-01-13 14:50:44 -0800

    Separate datetime tests

---
+++ cpaplib_tests/DateTimeTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cpaplib_tests
+{
+    [TestClass]
+    public class DateTimeTests
+    {
+        [TestMethod]
+        public void CanDetectDaylightSavingTime()
+        {
+            DateTime testDateTime = new(2024, 5, 15, 14, 31, 0, DateTimeKind.Local);
+            var isDaylightSavings = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time").IsDaylightSavingTime(testDateTime);
+            Assert.IsTrue(isDaylightSavings, $"Date '{testDateTime}' ({testDateTime.Kind}), isDaylightSavings = {isDaylightSavings}.");
+
+            testDateTime = new DateTime(2024, 2, 15, 14, 31, 0, DateTimeKind.Local);
+            isDaylightSavings = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time").IsDaylightSavingTime(testDateTime);
+            Assert.IsFalse(isDaylightSavings, $"Date '{testDateTime}' ({testDateTime.Kind}), isDaylightSavings = {isDaylightSavings}.");
+        }
+
+        [TestMethod]
+        public void DateTimeFromGoogleAPI()
+        {
+            var startTime = DateTime.UnixEpoch.ToLocalTime().AddMilliseconds(1700028990000);
+            var endTime = DateTime.UnixEpoch.ToLocalTime().AddMilliseconds(1700055270000);
+
+            var compareEndTime = new DateTime(1970, 1, 1).ToLocalTime().AddMilliseconds(1700055270000);
+
+            Assert.AreEqual(endTime, compareEndTime);
+
+            Debug.WriteLine($"Start: {startTime},    End: {endTime},    Duration: {endTime - startTime}");
+        }
+    }
+}
